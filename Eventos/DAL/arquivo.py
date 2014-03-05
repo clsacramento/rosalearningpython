55fa520981b05ff457b7a82fa2331ad64fd88f26
Rosa <rosamariasacramento@gmail.com>
2014-03-05 22:30:39 +0100

    ok

--- Eventos/DAL/arquivo.py
+++ Eventos/DAL/arquivo.py
@@ -2,5 +2,5 @@ import csv
 c=csv.writer(open("NOMES.csv","a"))
 # c.writerow(["Cynthia","Lopes"])
 # c.writerow(["Rosa","mae"])
-c.writerow(["Roberto","Biticos"]) #oi
+c.writerow(["Roberto","Biticos"]) #ok
 
